6c1168d1fdef7aa892636226db9475e31e280e05
Vandevelden <vandevelden@gmx.us>
2018-06-23 22:27:11 +0100

    test 3

--- contracts/challenge.sol
+++ contracts/challenge.sol
@@ -41,24 +41,25 @@ contract FitbitChallenges {
         string _cause,
         uint _data,
         uint64 _deadline    
-    )
+    ) public
     returns(uint)
     {
-        fitbitChallenges.push(Challenge(msg.sender,_companyaddress, _deadline, _cause,  _data, ChallengeStatus.CREATED, msg.value));
+        fitbitChallenges.push(Challenge(msg.sender,_companyaddress, _deadline, _cause,  _data, ChallengeStatus.CREATED, 0));
         return fitbitChallenges.length - 1;
     }
 
     function acceptChallenge(
         uint _challengeId
-    )
+    )public payable 
     {
         fitbitChallenges[_challengeId].status =  ChallengeStatus.ACCEPTED;
-
+        fitbitChallenges[_challengeId].amount = msg.value ;
     }
 
     function rejectChallenge(
         uint _challengeId
     )
+    public
     {
         fitbitChallenges[_challengeId].status =  ChallengeStatus.CANCELLED;
     }
@@ -67,6 +68,7 @@ contract FitbitChallenges {
         uint _challengeId,
         uint _data /** fitbit data will trigger this function*/
     )
+    public
     {
         require( fitbitChallenges[_challengeId].status == ChallengeStatus.ACCEPTED);
         require( _data > fitbitChallenges[_challengeId].data);
